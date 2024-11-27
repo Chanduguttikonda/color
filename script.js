138c9ba900852dadc3b51528225f0e565d49b223
Chanduguttikonda <chanduguttikonda123@gmail.com>
2024-11-27 16:05:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let redElement=document.getElementById('red');
+let blueElement=document.getElementById('blue');  
+function changecolor(red){
+    let textAreaElement=document.getElementById('texts');
+    textAreaElement.style.color=red;
+   
+} 
+function changecolor(blue,){
+    let textAreaElement=document.getElementById('texts');
+    textAreaElement.style.color=blue;
+} 
+function changecolor(yellow){
+    let textAreaElement=document.getElementById('texts');
+    textAreaElement.style.color=yellow;
+} 
+function changecolor(green){
+    let textAreaElement=document.getElementById('texts');
+    textAreaElement.style.color=green;
+} 
+function changecolor(orange){
+    let textAreaElement=document.getElementById('texts');
+    textAreaElement.style.color=purple;
+} 
+function changecolor(purple){
+    let textAreaElement=document.getElementById('texts');
+    textAreaElement.style.color=purple;
+} 
+function changebgcolor(red) {
+    let textAreaElement = document.getElementById('texts'); // Corrected the variable name
+    textAreaElement.style.backgroundColor = red; 
+}
+function changetext(Courier){
+    document.getElementById('texts').style.fontFamily=Courier;
+}
+function changetext(monospace){
+    document.getElementById('texts').style.fontFamily=monospace;
+}
+function changetext(helvetica){
+    document.getElementById('texts').style.fontFamily=helvetica;
+}
+function changetext(Times){
+    document.getElementById('texts').style.fontFamily=Times;
+}
+function changetext(Arial){
+    document.getElementById('texts').style.fontFamily=Arial;
+}
+function changetext(Georgia){
+    document.getElementById('texts').style.fontFamily=Georgia;
+}
+
